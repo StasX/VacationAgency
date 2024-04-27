f6ab615b0b139d83d3a80327827e9ae61cebeebf
StasX <s.mestechkin@gmail.com>
2024-04-27 20:31:57 +0300

    Add User Model and Role Model

--- statistics/server/src/auth/models.py
+++ statistics/server/src/auth/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+from django.db.models import Model, CharField,ForeignKey
+
 
 # Create your models here.
+class UserModel(Model):
+    first_name=CharField()
+    last_name=CharField()
+    email=CharField()
+    password=CharField()
+    role_id=ForeignKey(Role) 
+    class Meta:
+        db_table: "users"
+
+#------------------------------------------------------------------------------------------
+
+class RoleModel(Model):
+    role_type=CharField()
+    def __str__(self):
+        return self.role_type
+    class Meta:
+        db_table: "roles"
