e7baf2844d3d3dd678ffe78effc3071582f504ff
StasX <s.mestechkin@gmail.com>
2024-04-29 23:31:02 +0300

    Fix CountryModel

--- statistics/server/src/vacations/models.py
+++ statistics/server/src/vacations/models.py
@@ -1,19 +1,5 @@
-from django.db.models import Model, CharField, DateField, FloatField, ForeignKey
+from django.db.models import Model, CharField, DateField, FloatField, ForeignKey, RESTRICT
 from datetime import datetime
-
-
-class VacationModel(Model):
-    country = ForeignKey(CountryModel)
-    description = CharField(max_length=500)
-    start_date = DateField(default=datetime.now)
-    end_date = DateField(default=datetime.now)
-    price = FloatField(max_digits=7, decimal_places=2)
-    image = CharField(max_length=50)
-
-    class Meta:
-        db_table = 'vacations'
-
-# ------------------------------------------------------------------------------------------
 
 
 class CountryModel(Model):
@@ -27,3 +13,16 @@ class CountryModel(Model):
 
 # ------------------------------------------------------------------------------------------
 
+
+class VacationModel(Model):
+    country_id = ForeignKey(CountryModel, on_delete=RESTRICT)
+    description = CharField(max_length=500)
+    start_date = DateField(default=datetime.now)
+    end_date = DateField(default=datetime.now)
+    price = FloatField(max_digits=7, decimal_places=2)
+    image = CharField(max_length=50)
+
+    class Meta:
+        db_table = 'vacations'
+
+# ------------------------------------------------------------------------------------------
